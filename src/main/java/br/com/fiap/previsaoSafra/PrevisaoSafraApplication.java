c6dc7fe7a78334c704821693dfdffc2fcde0a4c7
Daniel <da442494@gmail.com>
2024-09-13 09:44:21 -0300

    agora vai

--- src/main/java/br/com/fiap/previsaoSafra/PrevisaoSafraApplication.java
+++ src/main/java/br/com/fiap/previsaoSafra/PrevisaoSafraApplication.java
@@ -2,10 +2,20 @@ package br.com.fiap.previsaoSafra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class PrevisaoSafraApplication {
 
+	@GetMapping
+	public ResponseEntity<String> referencia(){
+		return ResponseEntity.ok("Salve");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(PrevisaoSafraApplication.class, args);
 	}
